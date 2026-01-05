f12641e5faaf39a32efecac4a23da96a92cd3d30
vini amaral <viniamaral2026@gmail.com>
2026-01-05 23:44:45 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve './

---
+++ src/app/dashboard/pages/visoes-dados/page.tsx
@@ -0,0 +1,4 @@
+// This is a placeholder file. The content will be filled in based on user requests.
+export default function VisoesDadosPage() {
+  return <div>Conteúdo para Visões de Dados em breve...</div>;
+}
