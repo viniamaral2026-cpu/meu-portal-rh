2c3d3a44b08a79e0f77d5af9e462b1e18d0b183b
vini amaral <viniamaral2026@gmail.com>
2026-01-06 12:17:27 +0000

    Runtime Error
    Invalid src prop (https://www.gov.br/assets/images/assina

--- next.config.ts
+++ next.config.ts
@@ -70,6 +70,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.gov.br',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
