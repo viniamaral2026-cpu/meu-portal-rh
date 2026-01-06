ef32f1833c5e64fa2a7354afcfe897f502b2812f
vini amaral <viniamaral2026@gmail.com>
2026-01-06 17:40:14 +0000

    isso Módulo não encontrado: Não foi possível resolver 'flags/next'  ta d

--- src/app/my-page/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/my-page.tsx, or your other component
-import { createFeatureFlag } from "../../flags";
-
-export default async function Page() {
-  const enabled = await createFeatureFlag("my_first_gate")(); //Disabled by default, edit in the Statsig console
-  return <div>myFeatureFlag is {enabled ? "on" : "off"}</div>
-};
-//Note: this is designed for server & middleware - check "Getting Started" for client-side details!
